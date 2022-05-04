6077a4e6ba870c027b94dd4efb5942d44de86c40
Pedro <pedro.developer.af@gmail.com>
2022-05-03 19:12:55 -0500

    Se conecta con los botones

--- js/index.js
+++ js/index.js
@@ -1,4 +1,3 @@
-let cadena = "hola como estas";
 let remplazar = {
     "a": "ai",
     "e": "enter",
@@ -7,18 +6,13 @@ let remplazar = {
     "u": "ufat"
 };
 
-let entri = "fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!"
 
-//console.log(palabra.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat"));
-
-function encriptar(cadena) {
-    return cadena.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
+function encriptar() {
+    let texto = document.getElementById("texto").value;
+    console.log(texto.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat"));
 }
 
-function desencriptar(entri) {
-    return entri.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
+function desencriptar() {
+    let texto = document.getElementById("texto").value;
+     console.log(texto.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u"));
 }
-
-console.log(encriptar(cadena));
-
-console.log(desencriptar(entri));
