037ad3fefbece1c84104715d05ad95627cf4c321
Pedro <pedro.developer.af@gmail.com>
2022-05-08 13:07:07 -0500

    Se agrega responsive CSS

--- js/index.js
+++ js/index.js
@@ -9,10 +9,16 @@ let remplazar = {
 
 function encriptar() {
     let texto = document.getElementById("texto").value;
+
     console.log(texto.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat"));
+
 }
 
 function desencriptar() {
     let texto = document.getElementById("texto").value;
-     console.log(texto.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u"));
+    if (texto === texto) {
+        alert("Su texto esta desencriptado ");
+    } else {
+        console.log(texto.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u"));
+    }
 }
