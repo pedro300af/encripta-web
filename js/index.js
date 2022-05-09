93e63d8c5d3623f00d802cc744cdcab6dca4103d
Pedro <pedro.developer.af@gmail.com>
2022-05-08 22:02:02 -0500

    Version v1.0.0

--- js/index.js
+++ js/index.js
@@ -9,16 +9,40 @@ let remplazar = {
 
 function encriptar() {
     let texto = document.getElementById("texto").value;
-
-    console.log(texto.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat"));
-
+    if (texto == "") {
+        alert("Ingresar el texto, por favor");
+    } else {
+        textoNuevo = texto.replace(/a/g, "ai").replace(/e/g, "enter").replace(/i/g, "imes").replace(/o/g, "ober").replace(/u/g, "ufat");
+        let cambiar = document.querySelector(".mostrar-texto");
+        cambiar.innerHTML = textoNuevo;
+    }
 }
 
 function desencriptar() {
     let texto = document.getElementById("texto").value;
-    if (texto === texto) {
-        alert("Su texto esta desencriptado ");
+
+    if (texto == "") {
+        alert("Ingresar el texto, por favor");
     } else {
-        console.log(texto.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u"));
+        let textoNuevo = texto.replace(/ai/g, "a").replace(/enter/g, "e").replace(/imes/g, "i").replace(/ober/g, "o").replace(/ufat/g, "u");
+
+        let cambiar = document.querySelector(".mostrar-texto")
+        cambiar.innerHTML = textoNuevo;
     }
+}
+
+function copiar() {
+    let texto = document.querySelector(".mostrar-texto").innerHTML;
+    navigator.clipboard.writeText(texto);
+}
+
+function ocultar() {
+
+    document.querySelector(".texto-secudnario2").innerHTML = "";
+    document.querySelector(".texto-secudario").innerHTML = "";
+    document.querySelector("#imagen-secundaria").style.display = 'none';
+}
+
+function mostrar() {
+    document.querySelector(".boton-copiar").style.display = "inline";
 }
